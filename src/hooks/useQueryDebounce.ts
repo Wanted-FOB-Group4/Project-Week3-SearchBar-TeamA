459b3cc9d6c53c1cdfb572c1f98355b992b41551
lazy-sky <1cielo@naver.com>
2022-05-19 17:32:27 +0900

    Feat: Set default value about debounce delay

--- src/hooks/useQueryDebounce.ts
+++ src/hooks/useQueryDebounce.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-const useQueryDebounce = (value: string, delay: number) => {
+const useQueryDebounce = (value: string, delay: number = 300) => {
   const [debounceValue, setDebounceValue] = useState(value)
 
   useEffect(() => {
