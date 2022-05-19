8142d5325eb3763160fd74a4adebeb82baf9e887
lazy-sky <1cielo@naver.com>
2022-05-19 19:43:48 +0900

    Comment: Memo todos

--- src/components/KeywordRecommendItem/index.tsx
+++ src/components/KeywordRecommendItem/index.tsx
@@ -1,10 +1,10 @@
+import { useDispatch } from 'react-redux'
 import cx from 'classnames'
 
+import { ISearchState, setSearchToggle, setSearchWord } from 'store/slices/searchSlice'
 import { MagnifyingGlassIcon } from 'assets/svgs'
 
 import styles from './KeywordRecommendItem.module.scss'
-import { useDispatch } from 'react-redux'
-import { ISearchState, setSearchToggle, setSearchWord } from 'store/slices/searchSlice'
 
 interface SearchKeywordRecommendItemProps {
   resultData: any
@@ -14,6 +14,7 @@ interface SearchKeywordRecommendItemProps {
 const KeywordRecommendItem = ({ resultData, isFocusTrue }: SearchKeywordRecommendItemProps) => {
   const dispatch = useDispatch()
 
+  // TODO: 라우팅되도록. 예제 사이트 보시면 추천 검색어 클릭시 결과 페이지로 넘어갑니다.
   const handleKeywordClick = () => {
     dispatch(setSearchWord({ keyword: resultData.sickNm } as ISearchState))
     dispatch(setSearchToggle({ isOpen: false } as ISearchState))
