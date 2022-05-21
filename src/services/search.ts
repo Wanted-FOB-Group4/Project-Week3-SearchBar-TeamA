f5f42274f0cbd73306dca11d7ec740e974971a03
WOOJINLEEdev <lwj3794@naver.com>
2022-05-22 01:05:48 +0900

    Feat: api base url 환경변수로 분리

--- src/services/search.ts
+++ src/services/search.ts
@@ -2,10 +2,6 @@ import axios from 'axios'
 
 import { IDisease } from 'types/search'
 
-interface DiseaseListResponse {
-  item: IDisease[]
-}
-
 let apiCallCount = 0
 
 export const getDiseaseData = async (keyword: string) => {
@@ -15,9 +11,9 @@ export const getDiseaseData = async (keyword: string) => {
   // eslint-disable-next-line no-console
   console.log('API 호출 횟수:', apiCallCount)
 
-  const {
-    data: { item },
-  } = await axios.get<DiseaseListResponse>('/diseaseList.json')
+  const { data } = await axios.get<IDisease[]>(
+    `${process.env.REACT_APP_DISEASE_INFO_API_BASE_URL}/api/v1/search-conditions/?name=${keyword}`
+  )
 
-  return item
+  return data
 }
