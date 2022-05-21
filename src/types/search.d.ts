88d67c50dd42dc9a733c0d361363c5ea6c28236f
WOOJINLEEdev <lwj3794@naver.com>
2022-05-21 10:08:35 +0900

    Feat: type 추가

--- src/types/search.d.ts
+++ src/types/search.d.ts
@@ -2,5 +2,9 @@ export interface IDisease {
   sickCd: string
   sickNm: string
 }
+export interface IFuzzyDisease extends IDisease {
+  disease: string
+  longestDistance: number
+}
 
 export type RootState = ReturnType<typeof store.getState>
